48a2e8cbaddd5f277cf9a9f0226d427cb8b2d54f
pangratz <pangratz666@gmail.com>
2011-06-06 00:13:51 +0200

    changed implementation and added test

--- src/main/java/at/jku/oracle/Oracle.java
+++ src/main/java/at/jku/oracle/Oracle.java
@@ -1,30 +1,79 @@
 package at.jku.oracle;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 public class Oracle {
 
-	private enum Restaurant {
+	public static enum Restaurant {
 		McDonalds, A2ChinaRestaurant, JKUMensa, KHGMensa
 	}
 
 	public static void main(String[] args) {
 
-		Random r = new Random();
-		String choice;
-
-		if (args.length > 0) {
-			if (args.length == 1 && "nogs".equalsIgnoreCase(args[0])) {
-				choice = Restaurant.A2ChinaRestaurant.name();
-			} else {
-				choice = args[r.nextInt(args.length)];
-			}
-		} else {
-			Restaurant[] choices = Restaurant.values();
-			choice = choices[r.nextInt(choices.length)].name();
-		}
+		Oracle oracle = new Oracle();
+		String choice = oracle.ask(args);
 
 		System.out.println("Today's best choice is: " + choice);
 
+	}
+
+	private final Options options;
+	private final Random r;
+	private final String[] defaultRestaurants;
+
+	public Oracle() {
+		super();
+
+		r = new Random();
+
+		Restaurant[] values = Restaurant.values();
+		List<String> restaurantNames = new ArrayList<String>(values.length);
+		for (Restaurant restaurant : values) {
+			restaurantNames.add(restaurant.name());
+		}
+		defaultRestaurants = restaurantNames.toArray(new String[values.length]);
+
+		Option nogsOpt = OptionBuilder.create("nogs");
+		Option ownRestaurantsOpt = OptionBuilder.withDescription("alternative restaurants").hasOptionalArgs()
+				.create("a");
+
+		options = new Options();
+		options.addOption(ownRestaurantsOpt);
+		options.addOption(nogsOpt);
+	}
+
+	public String ask(String... args) {
+		// create the parser
+		CommandLineParser parser = new GnuParser();
+		try {
+			// parse the command line arguments
+			CommandLine line = parser.parse(options, args);
+
+			if (line.hasOption("nogs")) {
+				return Restaurant.A2ChinaRestaurant.name();
+			} else if (line.hasOption("a")) {
+				String[] restaurants = line.getOptionValues("a");
+				return selectRandom(restaurants);
+			} else {
+				return selectRandom(defaultRestaurants);
+			}
+
+		} catch (ParseException exp) {
+			throw new IllegalStateException("Parsing failed.  Reason: " + exp.getMessage(), exp);
+		}
+	}
+
+	private String selectRandom(String[] args) {
+		return args[r.nextInt(args.length)];
 	}
 }
--- src/test/java/at/jku/oracle/OracleTest.java
+++ src/test/java/at/jku/oracle/OracleTest.java
@@ -1,7 +1,58 @@
 package at.jku.oracle;
 
+import java.util.Arrays;
+
 import junit.framework.TestCase;
+import at.jku.oracle.Oracle.Restaurant;
 
 public class OracleTest extends TestCase {
+
+	private Oracle oracle;
+
+	public void testWithNoArgs() {
+		String chosenRestaurant = oracle.ask();
+		assertNotNull(chosenRestaurant);
+
+		try {
+			Restaurant restaurant = Restaurant.valueOf(chosenRestaurant);
+			assertNotNull(restaurant);
+		} catch (EnumConstantNotPresentException ecnpe) {
+			fail("should not raise an exception");
+		}
+	}
+
+	public void testWithNogsArg() {
+
+		String chosenRestaurant = oracle.ask("-nogs");
+		assertNotNull(chosenRestaurant);
+		assertEquals(Restaurant.A2ChinaRestaurant.name(), chosenRestaurant);
+
+	}
+
+	public void testWithOwnRestaurants() {
+		String chosenRestaurant = oracle.ask("-a", "uno", "dos", "tres");
+		assertNotNull(chosenRestaurant);
+
+		String[] restaurants = new String[] { "uno", "dos", "tres" };
+		Arrays.sort(restaurants);
+		int index = Arrays.binarySearch(restaurants, chosenRestaurant);
+
+		assertTrue(index >= 0);
+		assertEquals(restaurants[index], chosenRestaurant);
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+
+		oracle = new Oracle();
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+
+		oracle = null;
+	}
 
 }
