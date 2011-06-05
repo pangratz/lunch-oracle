bbbef19b63c9c359c721b7eb76e15ad76e9eb226
pangratz <pangratz666@gmail.com>
2011-06-05 22:53:48 +0200

    fixed reoccurred "nogs"-heisenbug

--- src/at/jku/oracle/Oracle.java
+++ src/at/jku/oracle/Oracle.java
@@ -4,23 +4,27 @@ import java.util.Random;
 
 public class Oracle {
 
-  private enum Restaurant {
-    McDonalds, A2ChinaRestaurant, JKUMensa, KHGMensa
-  }
+	private enum Restaurant {
+		McDonalds, A2ChinaRestaurant, JKUMensa, KHGMensa
+	}
 
-  public static void main(String[] args) {
+	public static void main(String[] args) {
 
-    Random r = new Random();
-    String choice;
+		Random r = new Random();
+		String choice;
 
-    if (args.length > 0) {
-      choice = args[r.nextInt(args.length)];
-    } else {
-      Restaurant[] choices = Restaurant.values();
-      choice = choices[r.nextInt(choices.length)].name();
-    }
+		if (args.length > 0) {
+			if (args.length == 1 && "nogs".equalsIgnoreCase(args[0])) {
+				choice = Restaurant.A2ChinaRestaurant.name();
+			} else {
+				choice = args[r.nextInt(args.length)];
+			}
+		} else {
+			Restaurant[] choices = Restaurant.values();
+			choice = choices[r.nextInt(choices.length)].name();
+		}
 
-    System.out.println("Today's best choice is: " + choice);
+		System.out.println("Today's best choice is: " + choice);
 
-  }
+	}
 }
